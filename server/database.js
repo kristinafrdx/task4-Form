06a0960c5e8a7aaf1f00040075500eebdf0aced5
kristinafrdx <kristina.frdx@mail.ru>
2024-02-13 00:38:34 +0300

    serv

--- server/database.js
+++ server/database.js
@@ -7,7 +7,6 @@ const pool = mysql.createPool({
   database: 'usersTable',
 }).promise()
 
-// get data from database
 export const status = {
   active: 'active',
   blocked: 'blocked',
@@ -47,27 +46,45 @@ export const createUser = async (name, login, status, password) => {
 }
 
 export const deleteUser = async (id) => {
-  try {
-    const deleted = await pool.query(`
+    await pool.query(`
       DELETE FROM users
       WHERE id = ?`,
       [id])
     return deleted;
-  }
-  catch (e) {
-    console.log(e)
-  }
 }
-
 export const deleteAll = async (users) => {
-  try {
-    const deletedAll = await pool.query(`
-    DELETE FROM users`
-    );
-    return deleteAll;
-  }
-  catch (e) {
-    console.log(e)
-  }
+    await pool.query(`
+    DELETE FROM users
+    WHERE id IN (?)`
+    , [users]);
+    return await getUsers();
 }
 
+export const blockAndUnblockUsers = async (status, usersId) => {
+    for (const item of usersId) {
+      await pool.execute(`
+    UPDATE users SET status = ?
+    WHERE id IN (?)`
+    , [status, item]);
+    }
+    return await getUsers();
+}
+
+export const checkLogin = async (login) => {
+    const [data] = await pool.query(
+      `SELECT * 
+      FROM users
+      WHERE login = ?`
+      , [login])
+      return data;
+}
+
+export const checkBlock = async (login, status) => {
+  const [data] = await pool.query(
+    `SELECT * 
+    FROM users
+    WHERE login = ? 
+    && status = ?`
+    , [login, status])
+    return data;
+}
--- server/serve.js
+++ server/serve.js
@@ -1,37 +1,64 @@
 import express from "express";
 import path from 'path';
 import cors from 'cors';
-import { getUsers, getUser, deleteUser, status, createUser, isExistUser, deleteAll } from './database.js'
+import { getUsers, status, createUser, isExistUser, deleteAll, blockAndUnblockUsers, checkLogin, checkBlock } from './database.js'
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-// get users from table
 app.get('/users', async (req, res) => {
   const users = await getUsers();
   res.send(users);
+  res.end()
 })
 
-// check within login
 app.post('/login', async (req, res) => {
   const user = req.body;
-  const existUser = await isExistUser(user.login, user.password)
-  console.log(existUser);
-  if (existUser) {
-    res.json({ message: 'true' })
+  const existUser = await isExistUser(user.login, user.password);
+  const blockCh = await checkBlock(user.login, 'blocked');
+  if (existUser && blockCh.length < 1) {
+    res.json({ message: 'true' });
   } else {
-    res.json({message: 'false' })
+    res.json({message: 'false' });
   }
 })
 
-// create new user in table
 app.post('/registration', async (req, res) => {
   const newUser = req.body;
-  await createUser(newUser.name, newUser.login, status.active, newUser.password)
+  const currentLogin = newUser.login;
+  const check = await checkLogin(currentLogin);
+  if (check.length > 0) {
+    res.json({ message: false})
+  } else {
+    res.json({ message: true})
+    await createUser(newUser.name, newUser.login, status.active, newUser.password);
+    res.end();
+  }
 });
+
+app.post('/table/delete', async (req, res) => {
+  const users = req.body
+  const id = users.map((el) => el.id)
+  const update = await deleteAll(id);
+  res.send(update);
+})
+
+app.post('/table/block', async (req, res) => {
+  const users = req.body;
+  const idUsers = users.map((el) => Number(el.id));
+  const update = await blockAndUnblockUsers('blocked', idUsers);
+  res.send(update);
+})
+
+app.post('/table/unlock', async (req, res) => {
+  const users = req.body;
+  const idUsers = users.map((el) => Number(el.id));
+  const update = await blockAndUnblockUsers('active', idUsers);
+  res.send(update);
+})
 
 app.listen(PORT, () => {
   console.log(`server is running on: http://localhost:${PORT}`)
